10dc36768109e0bd359a0758c79aebe13303e402
KevinJuly95 <32257783+KevinJuly95@users.noreply.github.com>
2018-03-26 14:21:10 +0800

    Mengisi Isi Atribut

--- src/kevin/keretaaksi.java
+++ src/kevin/keretaaksi.java
@@ -14,5 +14,23 @@ public class keretaaksi {
         KeretaApi Listrik = new KeretaApi();
         KeretaApi Lokomotif = new KeretaApi();
         KeretaApi Levitasi = new KeretaApi();
+        
+        Listrik.Jenis = "KRL Hitachi";
+        Listrik.JmlKursi = 80;
+        Listrik.JmlGerbang = 8;
+        Listrik.kecepatan = 120;
+        Listrik.JmlAwak = 28;
+        
+        Lokomotif.Jenis = "Lokomotif CC300";
+        Lokomotif.JmlKursi = 60;
+        Lokomotif.JmlGerbang = 8;
+        Lokomotif.kecepatan = 120;
+        Lokomotif.JmlAwak = 28;
+        
+        Levitasi.Jenis = "KRL Hitachi";
+        Levitasi.JmlKursi = 80;
+        Levitasi.JmlGerbang = 8;
+        Levitasi.kecepatan = 120;
+        Levitasi.JmlAwak = 28;
     }
 }
