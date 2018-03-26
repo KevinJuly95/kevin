9d031cd13a74781292a0275fb9ea0712b4bf1347
KevinJuly95 <32257783+KevinJuly95@users.noreply.github.com>
2018-03-26 12:54:19 +0800

    Membuat Atribut dan Behavior

--- src/kevin/KeretaApi.java
+++ src/kevin/KeretaApi.java
@@ -10,5 +10,19 @@ package kevin;
  * @author ACER
  */
 public class KeretaApi {
+    String Jenis;
+    int JmlKursi;
+    int JmlGerbang;
+    int kecepatan;
+    int JmlAwak;
     
+    void datakereta(){
+        System.out.println("+++++++++++++++++++++++++++++++++++++");
+        System.out.println("Jenis Kereta    : "+Jenis);
+        System.out.println("Jumlah Kursi    : "+JmlKursi);
+        System.out.println("Jumlah Gerbang  : "+JmlGerbang);
+        System.out.println("Kecepatan Maks  : "+kecepatan+"Km/Jam");
+        System.out.println("Jumlah Awak     : "+JmlAwak);
+        System.out.println("+++++++++++++++++++++++++++++++++++++");
+    }
 }
